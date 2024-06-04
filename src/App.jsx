c3e1953061fab9772899062046eec4f69886c035
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-06-04 16:55:35 -0700

    updated imports and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/home-page/Navbar'
-import Hero2 from './components/home-page/Hero2'
-import LogoScroller from './components/home-page/LogoScroller'
-import Productscroller from './components/home-page/Productscroller'
-import Banner1 from './components/home-page/Banner1'
-import Footer2 from './components/home-page/Footer2'
+import HomePage from './components/HomePage'
 
 export default function App() {
   return (
     <div className='app'>
-      <Navbar /> 
-      <Hero2 />
-      <LogoScroller />
-      <Productscroller />
-      <Banner1 />
-      <Productscroller />
-      <Productscroller />
-      <Footer2 />
+      <HomePage />
     </div>
   )
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Navbar from './home-page/Navbar'
+import Hero2 from './home-page/Hero2'
+import LogoScroller from './home-page/LogoScroller'
+import Productscroller from './home-page/Productscroller'
+import Banner1 from './home-page/Banner1'
+import Footer2 from './home-page/Footer2'
+
+export default function HomePage() {
+    return (
+        <>
+            <Navbar /> 
+            <Hero2 />
+            <LogoScroller />
+            <Productscroller />
+            <Banner1 />
+            <Productscroller />
+            <Productscroller />
+            <Footer2 />
+        </>
+    )
+}
