f8110f6a4c975cfeb6de64d6f81004b75245c2a6
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-06-05 11:34:59 -0700

    added sliding animations to login form

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 import './Authenticate.css'
 import img from './assets/login-background.png'
-import { MdLockOutline } from "react-icons/md";
+import { MdLockOutline } from "react-icons/md"
 
 export default function Login() {
+    const [active, setActive] = useState(false)
+
+    function slide() {
+        setActive(!active)
+    }
+
     return (
         <div className='login-page'>
-            <div className="login-img">
+            <div className={active ? 'login-img-active' : 'login-img'}>
                 <img src={img} />
             </div>
-            <form className="login-form">
+            <form className={active ? 'register-form-active auth-form' : 'register-form auth-form'}>
+                <div className='lock-icon-container'>
+                    <MdLockOutline className='lock-icon' />
+                </div>
+                <h1>Sign up</h1>
+                <input placeholder='Email Address*' type='email' required />
+                <input placeholder='Password*' type='password' required />
+                <button>SIGN UP</button>
+                <div className='login-links'>
+                    <p>Forgot Password?</p>
+                    <p onClick={slide}>Already Have An Account? Sign In</p>
+                </div>
+            </form>
+            <form className={active ? 'login-form-active auth-form' : 'login-form auth-form'}>
                 <div className='lock-icon-container'>
                     <MdLockOutline className='lock-icon' />
                 </div>
@@ -19,7 +39,7 @@ export default function Login() {
                 <button>SIGN IN</button>
                 <div className='login-links'>
                     <p>Forgot Password?</p>
-                    <p>Don't Have An Account? Sign Up</p>
+                    <p onClick={slide}>Don't Have An Account? Sign Up</p>
                 </div>
             </form>
         </div>
