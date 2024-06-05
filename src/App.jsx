24705d5a5142a887ade90e1d5e383f585e374667
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-06-04 17:32:09 -0700

    linked navbar to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
+import Login from './components/authentication/Login'
 
 export default function App() {
   return (
-    <div className='app'>
-      <HomePage />
-    </div>
+    <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} exact />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+    </Router>
   )
 }
