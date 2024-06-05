a51f716a3661020e1a9b5261182bab5235ba97ce
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-06-05 09:03:24 -0700

    added style to login form

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -13,10 +13,14 @@ export default function Login() {
                 <div className='lock-icon-container'>
                     <MdLockOutline className='lock-icon' />
                 </div>
-                <h1>Login</h1>
-                <input />
-                <input />
+                <h1>Sign in</h1>
+                <input placeholder='Email Address*' type='email' required />
+                <input placeholder='Password*' type='password' required />
                 <button>SIGN IN</button>
+                <div className='login-links'>
+                    <p>Forgot Password?</p>
+                    <p>Don't Have An Account? Sign Up</p>
+                </div>
             </form>
         </div>
     )
