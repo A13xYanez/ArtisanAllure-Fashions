e951c37f2125aa1306195ec7229164afb075f196
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-06-14 15:16:04 -0700

    created user schema

---
+++ back-end/src/db/users.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    user_info: {
+        first_name: { type: String, required: true },
+        last_name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        },
+    authentication: {
+        password: { type: String, required: true, select: false },
+        salt: { type: String, required: true, select: false },
+        session_token: { type: String, select: false },
+    },
+        });
+
+export const UserModel = mongoose.model('User', userSchema);
