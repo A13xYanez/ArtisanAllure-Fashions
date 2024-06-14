9059468e1e30a753c6bb4df867e633f807507fa8
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-06-14 15:09:02 -0700

    connected to mongodb

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,8 +1,53 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import pkg from 'mongoose';
+const { Promise } = pkg;
+import router from './router/index.js';
 
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+// Rate limiter middleware to limit the number of requests per IP
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // 100 requests per 15 minutes per IP
+    message: 'Too many requests from this IP, please try again after some time',
 });
+
+// Express app
+const app = express();
+// app.use(limiter);
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+// HTTP server
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
+
+// MongoDB connection
+const MONGO_URL = 'mongodb://localhost:27017/ArtisanAllureFashion';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to MongoDB', err);
+});
+
+app.use('/', router());
