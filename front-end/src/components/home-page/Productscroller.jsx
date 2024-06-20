fabcdf1380162b0ce2cd5c8ea4c50ed4f43d291f
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-06-19 17:56:24 -0700

    connected products from db to homepage

--- front-end/src/components/home-page/Productscroller.jsx
+++ front-end/src/components/home-page/Productscroller.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import { GoStarFill } from "react-icons/go";
 import { BsFillCartPlusFill } from "react-icons/bs";
 import { BsFillCartCheckFill } from "react-icons/bs";
 import { FaRegHeart } from "react-icons/fa";
 import { IoIosArrowForward } from "react-icons/io";
-import img from './assets/clothes/47-BRAND-Los-Angeles-Dodgers-47-Clean-Up-Strapback-Hat.jpg'
 
 export default function Productscroller() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/home/featured')
+        .then((res) => { setProducts(res.data); })
+        .catch((error) => { console.error(error.response.data.error); });
+    }, []);
+
     return (
         <section className="product">
             <div className="section-header">
@@ -18,196 +27,36 @@ export default function Productscroller() {
             <button className="pre-btn"></button>
             <button className="nxt-btn"></button>
             <div className="product-container">
-                <div className="product-card">
-                    <div className="product-image">
-                        <div className="heart-container">
-                            <FaRegHeart className="heart-icon" />
+                {products.map((product) => (
+                    <div className="product-card">
+                        <div className="product-image">
+                            <div className="heart-container">
+                                <FaRegHeart className="heart-icon" />
+                            </div>
+                            <span className="discount-tag">50% off</span>
+                            <img src={product.product_image} className="product-thumb" alt="" />
+                            <button class="card-btn">add to wishlist</button>
                         </div>
-                        <span className="discount-tag">50% off</span>
-                        <img src={img} className="product-thumb" alt="" />
-                        <button class="card-btn">add to wishlist</button>
-                    </div>
-                    <div className="product-info">
-                        <div className="info-title">
-                            <h2 className="product-brand">brand</h2>
-                            <div className="review-stars">
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
+                        <div className="product-info">
+                            <div className="info-title">
+                                <h2 className="product-brand">brand</h2>
+                                <div className="review-stars">
+                                    <GoStarFill />
+                                    <GoStarFill />
+                                    <GoStarFill />
+                                    <GoStarFill />
+                                    <GoStarFill />
+                                </div>
+                            </div>
+                            <p className="product-short-description">{product.product_name}</p>
+                            <span className="actual-price">${product.regular_price}</span><span className="price">${product.sale_price}</span>
+                            <div className="cart-container">
+                                <BsFillCartPlusFill className="cart-icon" />
                             </div>
                         </div>
-                        <p className="product-short-description">Los Angeles Dodgers '47 Strapback</p>
-                        <span className="actual-price">$40</span><span className="price">$200</span>
-                        <div className="cart-container">
-                            <BsFillCartPlusFill className="cart-icon" />
-                        </div>
                     </div>
-                </div>
-                <div className="product-card">
-                    <div className="product-image">
-                        <div className="heart-container">
-                            <FaRegHeart className="heart-icon" />
-                        </div>
-                        <span className="discount-tag">50% off</span>
-                        <img src={img} className="product-thumb" alt="" />
-                        <button class="card-btn">add to wishlist</button>
-                    </div>
-                    <div className="product-info">
-                        <div className="info-title">
-                            <h2 className="product-brand">brand</h2>
-                            <div className="review-stars">
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                            </div>
-                        </div>
-                        <p className="product-short-description">Los Angeles Dodgers '47 Strapback Hat</p>
-                        <span className="actual-price">$40</span><span className="price">$20</span>
-                        <div className="cart-container">
-                            <BsFillCartPlusFill className="cart-icon" />
-                        </div>
-                    </div>
-                </div>
-                <div className="product-card">
-                    <div className="product-image">
-                        <div className="heart-container">
-                            <FaRegHeart className="heart-icon" />
-                        </div>
-                        <span className="discount-tag">50% off</span>
-                        <img src={img} className="product-thumb" alt="" />
-                        <button class="card-btn">add to wishlist</button>
-                    </div>
-                    <div className="product-info">
-                        <div className="info-title">
-                            <h2 className="product-brand">brand</h2>
-                            <div className="review-stars">
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                            </div>
-                        </div>
-                        <p className="product-short-description">Los Angeles Dodgers '47 Strapback Hat</p>
-                        <span className="actual-price">$40</span><span className="price">$20</span>
-                        <div className="cart-container">
-                            <BsFillCartPlusFill className="cart-icon" />
-                        </div>
-                    </div>
-                </div>
-                <div className="product-card">
-                    <div className="product-image">
-                        <div className="heart-container">
-                            <FaRegHeart className="heart-icon" />
-                        </div>
-                        <span className="discount-tag">50% off</span>
-                        <img src={img} className="product-thumb" alt="" />
-                        <button class="card-btn">add to wishlist</button>
-                    </div>
-                    <div className="product-info">
-                        <div className="info-title">
-                            <h2 className="product-brand">brand</h2>
-                            <div className="review-stars">
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                            </div>
-                        </div>
-                        <p className="product-short-description">Los Angeles Dodgers '47 Strapback Hat</p>
-                        <span className="actual-price">$40</span><span className="price">$20</span>
-                        <div className="cart-container">
-                            <BsFillCartPlusFill className="cart-icon" />
-                        </div>
-                    </div>
-                </div>
-                <div className="product-card">
-                    <div className="product-image">
-                        <div className="heart-container">
-                            <FaRegHeart className="heart-icon" />
-                        </div>
-                        <span className="discount-tag">50% off</span>
-                        <img src={img} className="product-thumb" alt="" />
-                        <button class="card-btn">add to wishlist</button>
-                    </div>
-                    <div className="product-info">
-                        <div className="info-title">
-                            <h2 className="product-brand">brand</h2>
-                            <div className="review-stars">
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                            </div>
-                        </div>
-                        <p className="product-short-description">Los Angeles Dodgers '47 Strapback Hat</p>
-                        <span className="actual-price">$40</span><span className="price">$20</span>
-                        <div className="cart-container">
-                            <BsFillCartPlusFill className="cart-icon" />
-                        </div>
-                    </div>
-                </div>
-                <div className="product-card">
-                    <div className="product-image">
-                        <div className="heart-container">
-                            <FaRegHeart className="heart-icon" />
-                        </div>
-                        <span className="discount-tag">50% off</span>
-                        <img src={img} className="product-thumb" alt="" />
-                        <button class="card-btn">add to wishlist</button>
-                    </div>
-                    <div className="product-info">
-                        <div className="info-title">
-                            <h2 className="product-brand">brand</h2>
-                            <div className="review-stars">
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                            </div>
-                        </div>
-                        <p className="product-short-description">Los Angeles Dodgers '47 Strapback Hat</p>
-                        <span className="actual-price">$40</span><span className="price">$20</span>
-                        <div className="cart-container">
-                            <BsFillCartPlusFill className="cart-icon" />
-                        </div>
-                    </div>
-                </div>
-                <div className="product-card">
-                    <div className="product-image">
-                        <div className="heart-container">
-                            <FaRegHeart className="heart-icon" />
-                        </div>
-                        <span className="discount-tag">50% off</span>
-                        <img src={img} className="product-thumb" alt="" />
-                        <button class="card-btn">add to wishlist</button>
-                    </div>
-                    <div className="product-info">
-                        <div className="info-title">
-                            <h2 className="product-brand">brand</h2>
-                            <div className="review-stars">
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                                <GoStarFill />
-                            </div>
-                        </div>
-                        <p className="product-short-description">Los Angeles Dodgers '47 Strapback Hat</p>
-                        <span className="actual-price">$40</span><span className="price">$20</span>
-                        <div className="cart-container">
-                            <BsFillCartPlusFill className="cart-icon" />
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     )
-}
+};
