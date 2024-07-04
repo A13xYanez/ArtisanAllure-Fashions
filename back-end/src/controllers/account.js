8fc3a303c082ad9d142a9539aa6a250bd9fd95b0
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-07-03 19:29:55 -0700

    completed products in cart retrieval

--- back-end/src/controllers/account.js
+++ back-end/src/controllers/account.js
@@ -64,3 +64,35 @@ export const addToAccountCart = async (req, res) => {
         res.status(500).send('Server Error');
     }
 };
+
+
+
+// Gets the user and the products they have in their
+// cart to display in the cart page
+export const displayUserProductsInCart = async (req, res) => {
+    try {
+        const user = get(req, 'identity');
+        const userInformation = await getUserById(user._id, false);
+        const itemsInCart = userInformation.account_details.cart.items;
+
+        const formattedProductsInCart = await Promise.all(itemsInCart.map(async product => {
+            const productInformation = await getProductById(product.item);
+            
+            return ({
+                product_image: productInformation.product_image,
+                product_name: productInformation.product_name,
+                brand: productInformation.brand,
+                ratings: productInformation.ratings,
+                regular_price: productInformation.regular_price,
+                sale_price: productInformation.sale_price,
+                id: productInformation._id,
+                quantity: product.quantity
+            })
+        }));
+
+        res.json(formattedProductsInCart);
+    } catch (error) {
+        console.error('Error displaying items in cart:', error);
+        res.status(500).send('Server Error');
+    }
+}
--- back-end/src/router/account.js
+++ back-end/src/router/account.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/authentication.js';
-import { addToAccountCart } from '../controllers/account.js';
+import { addToAccountCart, displayUserProductsInCart } from '../controllers/account.js';
 
 export default (router) => {
     router.post('/account/addToCart/:id', isAuthenticated, addToAccountCart);
+    router.get('/account/displayProductsInCart', isAuthenticated, displayUserProductsInCart);
 }
