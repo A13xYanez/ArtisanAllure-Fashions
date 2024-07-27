882a178c057a036c6c671a2fc5bd98fd19943644
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-07-26 17:36:58 -0700

    change star when icon when clicked on

--- front-end/src/components/product-showcase/WriteReview.jsx
+++ front-end/src/components/product-showcase/WriteReview.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState , useEffect} from 'react';
 import axios from 'axios';
 import { GoStarFill } from "react-icons/go";
+import { GoStar } from "react-icons/go";
 import { IoClose } from "react-icons/io5";
 
 export default function WriteReview({ setIsActive, id }) {
@@ -13,9 +14,11 @@ export default function WriteReview({ setIsActive, id }) {
     }
 
     function submitProductReview() {
-        axios.post(`http://localhost:8080/product/create/review/${id}`, { productReview, productRating })
-        .then((res) => setIsActive(false))
-        .catch((error) => { console.error(error); });
+        if (productRating != null && productReview != "") {
+            axios.post(`http://localhost:8080/product/create/review/${id}`, { productReview, productRating })
+            .then((res) => {setIsActive(false), window.location.reload()})
+            .catch((error) => { console.error(error); });
+        }
     }
 
     return (
@@ -28,17 +31,22 @@ export default function WriteReview({ setIsActive, id }) {
                     </div>
                 </div>
                 <div className="leave-rating">
-                    <h4>Overall rating</h4>
+                    <h4>Overall rating <span>*</span></h4>
                     <div className='leave-rating-stars-container'>
-                        <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(1)} />
-                        <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(2)} />
-                        <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(3)} />
-                        <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(4)} />
-                        <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(5)} />
+                        {productRating >= 1 ? <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(1)} /> 
+                        : <GoStar className='leave-rating-stars' onClick={() => setProductRating(1)} />}
+                        {productRating >= 2 ? <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(2)} /> 
+                        : <GoStar className='leave-rating-stars' onClick={() => setProductRating(2)} />}
+                        {productRating >= 3 ? <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(3)} /> 
+                        : <GoStar className='leave-rating-stars' onClick={() => setProductRating(3)} />}
+                        {productRating >= 4 ? <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(4)} /> 
+                        : <GoStar className='leave-rating-stars' onClick={() => setProductRating(4)} />}
+                        {productRating >= 5 ? <GoStarFill className='leave-rating-stars' onClick={() => setProductRating(5)} /> 
+                        : <GoStar className='leave-rating-stars' onClick={() => setProductRating(5)} />}
                     </div>
                 </div>
                 <div className="leave-review">
-                    <h4>Review</h4>
+                    <h4>Review <span>*</span></h4>
                     <textarea onChange={e => setProductReview(e.target.value)} required />
                 </div>
                 <div className="close-or-submit-review">
