c49304089bfef50dc9309dfe6305952f342f0f68
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-08-23 17:08:08 -0700

    attempt to get session cookie to work

--- back-end/src/controllers/authentication.js
+++ back-end/src/controllers/authentication.js
@@ -88,7 +88,7 @@ export const login = async (req, res) => {
     await user.save();
 
     res.cookie('session_token', user.authentication.session_token, {
-        domain: 'localhost',
+        domain: 'artisanallurefashions-frontend.onrender',
         path: '/',
         secure: true,
         httpOnly: true,
